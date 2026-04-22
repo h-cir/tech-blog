f5af3af65a73488fddf0dabc9a66122b6642e0d8
hin <jhiys92@gmail.com>
2026-04-22 17:44:43 +0900

    deploy: initialize Quartz blog with GitHub Pages workflow

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "h-cir.github.io/tech-blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
